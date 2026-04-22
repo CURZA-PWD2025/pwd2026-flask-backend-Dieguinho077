8409faf079580a29790805e0f02eb72eca6fcae7
Dieguinho077 <diegosmansilla05@gmail.com>
2026-04-22 12:29:59 -0300

    commit 22/4

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,38 +1,19 @@
-from dotenv import load_dotenv
-from flask import Flask
-from app.models import db
-from app.config import config
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.routes.user_routes import users
 from app.routes.rol_routes import roles
+from app.routes.categoria_routes import categorias
+from app.routes.producto_routes import productos
+from app.routes.proveedor_routes import proveedores
+from app.routes.movimiento_stock_routes import movimientos_stock
 from app.routes.auth_routes import auth_bp
-from flask_migrate import Migrate
-from flask_jwt_extended import JWTManager
+from flask import Blueprint
 
-load_dotenv(override = True)
-import os
-migrate = Migrate()
-jwt = JWTManager()
+api_v1 = Blueprint('api_v1', name, url_prefix='/api_v1')
 
-def create_app():
-    app = Flask(__name__)
-    env = os.getenv('FLASK_ENV', 'development')
-    app.config.from_object(config[env])
-    app.register_blueprint(users)
-    app.register_blueprint(roles)
-    app.register_blueprint(auth_bp)
-    
-    @app.route('/')
-    @app.route('/<nombre>')    
-    def home(nombre = None):
-        if (nombre == None):
-            return f' <h1>Hola  desde programacion web dinamica 2026<h1>'
-        return f'Hola {nombre} te saludamos desde programacion web dinamica 2026'
-
-    @app.route('/saludo')
-    def saludo():
-        return f'Hola desde programacion web dinamica 2026 saludo'
-    db.init_app(app)
-    migrate.init_app(app=app, db=db)
-    jwt.init_app(app)
-    return app
-    
+api_v1.register_blueprint(users,url_prefix='/users')
+api_v1.register_blueprint(roles,url_prefix='/roles')
+api_v1.register_blueprint(categorias,url_prefix='/categorias')
+api_v1.register_blueprint(productos, url_prefix='/productos')
+api_v1.register_blueprint(proveedores,url_prefix='/proveedores')
+api_v1.register_blueprint(movimientos_stock,url_prefix='/movimientos_stock')
+api_v1.register_blueprint(auth_bp,url_prefix='/auth')
--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,3 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+def init_models():
+    from .rol import Rol
+    from .user import User
+    from .categoria import Categoria
+    from .proveedor import Proveedor
+    from .producto import Producto
